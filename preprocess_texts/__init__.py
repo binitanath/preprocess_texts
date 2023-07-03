a7dfff7ba8724ee1137d74ac5bcbcd6e387e627a
binitanath <binitanath10@gmail.com>
2023-07-03 17:27:41 +0530

    initial commit

---
+++ preprocess_texts/__init__.py
@@ -0,0 +1,69 @@
+from preprocess_texts import utils
+
+__version__ ="0.0.1"
+
+def get_wordcounts(x):
+    return utils._get_avg_wordcount(x)
+
+def get_charcounts(x):
+    return utils._get_charcounts(x)
+
+def get_avg_wordcount(x):
+    return utils._get_avg_wordcount(x)
+
+def get_stopwords_count(x):
+    return utils._get_stopwords_count(x)
+
+def get_hashtag_count(x):
+    return utils._get_hashtag_count(x)
+
+def get_mention_count(x):
+    return utils._get_mention_count(x)
+
+def get_digit_count(x):
+    return utils._get_digit_count(x)
+
+def get_uppercaset_count(x):
+    return utils._get_uppercaset_count(x)
+
+def get_cont_extraction(x):
+    return utils._get_cont_extraction(x)
+
+def get_emails(x):
+    return utils._get_emails(x)
+
+def remove_emails(x):
+    return utils._remove_emails(x)
+
+def get_urls(x):
+    return utils._get_urls(x)
+
+def remove_urls(x):
+    return utils._remove_urls(x)
+
+def remove_rt(x):
+    return utils._remove_rt(x)
+
+def remove_specialchars(x):
+    return utils._remove_specialchars(x)
+
+def remove_htmltags(x):
+    return utils._remove_htmltags(x)
+
+def remove_accented_chars(x):
+    return utils._remove_accented_chars(x)
+
+def remove_stopwords(x):
+    return utils._remove_stopwords(x)
+
+def make_base(x):
+    return utils._make_base(x)
+
+def remove_commonwords(x, no=20):
+    return utils._remove_commonwords(x, no=20)
+
+def remove_rarewords(x, no = 20):
+    return utils._remove_rarewords(x, no=20)
+
+def spelling_correction(x):
+    return utils._spelling_correction(x)
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = "preprocess_texts",
+    version = "0.0.1",
+    author = "Binita Nath",
+    author_email="binitanath10@gmail.com",
+    description = "Text Preprocessing",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers =[
+        'Programming Language :: Python :: 3 ',
+        'License :: OSI Approved :: MIT License',
+        'Operating System:: OS Independent'
+    ],
+    python_requires = '>= 3'
+)   
